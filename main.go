ecaaa2d9003cb51747631b666648b9a0c536aad0
Ramish Hassan <ramishshah93@gmail.com>
2022-09-07 11:51:42 +0200

    making a crud api in go lang next

--- main.go
+++ main.go
@@ -1,45 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+// func formHandler(w http.ResponseWriter, r *http.Request){
+// 	if err := r.ParseForm(); err != nil {
+// 		fmt.Fprintf(w, "parse form error: %v", err)
+// 		return
+// 	}
+// 	fmt.Fprintf(w, "Post req successfully ")
+// 	name := r.FormValue("name")
+// 	address := r.FormValue("address")
+// 	fmt.Fprintf(w, "name = %s ", name)
+// 	fmt.Fprintf(w, "address = %s ", address)
 
-func formHandler(w http.ResponseWriter, r *http.Request){
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "parse form error: %v", err)
-		return 
-	}
-	fmt.Fprintf(w, "Post req successfully ")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "name = %s ", name)
-	fmt.Fprintf(w, "address = %s ", address)
+// }
 
-}
+// func helloHandler(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path != "/hello" {
+// 		http.Error(w, "404 Not Found", http.StatusNotFound)
+// 		return
+// 	}
+// 	if r.Method != "GET" {
+// 		http.Error(w, "method not supported", http.StatusNotFound)
+// 		return
+// 	}
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 Not Found", http.StatusNotFound)
-		return 
-	}
-	if r.Method != "GET" {
-		http.Error(w, "method not supported", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintf(w, "Hello Go")
-}
+// 	fmt.Fprintf(w, "Hello Go")
+// }
 
 func main () {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
+	// fileServer := http.FileServer(http.Dir("./static"))
+	// http.Handle("/", fileServer)
+	// http.HandleFunc("/form", formHandler)
+	// http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf("strting server at port 8080\n")
-	if err:= http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	// fmt.Printf("strting server at port 8080\n")
+	// if err:= http.ListenAndServe(":8080", nil); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
